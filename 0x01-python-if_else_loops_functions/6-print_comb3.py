794e8fa03ffd25250935ccb3c8914592ca11011f
OsmosisSE <sirosmosis1@gmail.com>
2023-11-28 15:51:17 -0800

    python

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for a in range(0, 90):
+    if a % 10 > a / 10:
+        if a != 89:
+            print("{:02d}, ".format(a), end='')
+        else:
+            print("{:02d}".format(a))
