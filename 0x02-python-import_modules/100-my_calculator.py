1bf11be33becc47ae0c786efe67c76b02923b706
OsmosisSE <sirosmosis1@gmail.com>
2023-12-04 08:56:43 -0800

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,4 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/python#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)3
 
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
