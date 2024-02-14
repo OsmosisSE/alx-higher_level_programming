81882807faa1479c9bafe6ea4b9372afcac3e59a
OsmosisSE <sirosmosis1@gmail.com>
2024-02-14 11:00:49 -0800

    my sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- Lists the number of records with the same score in the table second_table.
 -- Records are ordered by descending count.
-SELECT `score`, COUNT(*) AS `number`
+SELECT `score`, `name`
 FROM `second_table`
-GROUP BY `score`
-ORDER BY `number` DESC;
+WHEERE `name` != ""
+ORDER BY `score` DESC;
---
+++ README.md
@@ -0,0 +1,3 @@
+## Python
+
+0x00. Python - Hello, World
